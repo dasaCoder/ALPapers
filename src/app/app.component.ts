f908815d0b22a66f61bbad8161877c99621d33b4
dilushad <dilushad@zone24x7.com>
2020-03-05 15:31:50 +0530

    form validadtion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'al-test';
+  qType: string;
+  qNumber: string;
+  qSubject: string;
+
+  paperForm = new FormGroup({
+
+    qTypeValidator : new FormControl('', [Validators.required]),
+    qNumberValidator : new FormControl('',[Validators.required]),
+    qSubjectValidator : new FormControl('', [Validators.required])
+    
+  });
+
+  get qTypeValidator() : any {
+    return this.paperForm.get('qTypeValidator');
+  }
+
+  get qSubjectValidator() : any {
+    return this.paperForm.get('qSubjectValidator');
+  }
+
+  get qNumberValidator() : any {
+    return this.paperForm.get('qNumberValidator');
+  }
+
+  onFormSubmit() {
+    if(this.paperForm.valid) {
+
+      alert(this.paperForm.get('qNumberValidator').value);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
