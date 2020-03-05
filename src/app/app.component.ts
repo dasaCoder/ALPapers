c67f300c03d1183756f19611ee04688fcc99a4b4
dilushad <dilushad@zone24x7.com>
2020-03-06 01:03:23 +0530

    upload image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from 'angularfire2/storage';
+import { Observable, of } from 'rxjs';
+import {  finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +14,11 @@ export class AppComponent {
   qType: string;
   qNumber: string;
   qSubject: string;
+
+  task: AngularFireUploadTask;
+  ref: AngularFireStorageReference;
+  downloadURL :Observable<any>;
+  image:string;
 
   paperForm = new FormGroup({
 
@@ -32,11 +40,42 @@ export class AppComponent {
     return this.paperForm.get('qNumberValidator');
   }
 
+  constructor(private afStorage: AngularFireStorage) {
+
+  }
+
   onFormSubmit() {
     if(this.paperForm.valid) {
 
       alert(this.paperForm.get('qNumberValidator').value);
+      console.log(this.paperForm.value);
     }
+  }
+
+  upload(event) {
+      // create a random id
+    const randomId = Math.random().toString(36).substring(2);
+    // create a reference to the storage bucket location
+    this.ref = this.afStorage.ref(randomId);
+    // the put method creates an AngularFireUploadTask
+    // and kicks off the upload
+    this.task = this.ref.put(event.target.files[0]);
+    this.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.downloadURL = this.ref.getDownloadURL()
+        this.downloadURL.subscribe(url => {
+          this.image = url;
+          console.log(this.image);
+        });
+        
+      })
+    )
+    .subscribe(() => {
+
+      console.log(this.image);
+    });
+
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireStorageModule} from 'angularfire2/storage'
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -24,7 +27,14 @@ import {MatButtonModule} from '@angular/material/button';
     FlexLayoutModule,
     MatSelectModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDGk0Ji7Yt28nrFEcdjGmhsx7LmXC3Oj6I",
+      authDomain: "asia-south1",
+      storageBucket: "gs://examhelp-c87f6.appspot.com/",
+      projectId: "examhelp-c87f6",
+    }),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
